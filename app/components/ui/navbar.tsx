24f69c759fb2928aaa3dbc8760fdcee68efa820e
Ray Cook <rayscook@gmail.com>
2024-01-09 20:30:37 -0500

    first commit

---
+++ app/components/ui/navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+      <nav className="bg-slate-700 p-6 text-white">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-bold">RMNDR.ETH</h1>
+          <div>
+            {/* Add links or buttons here */}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+  
+export default Navbar;
